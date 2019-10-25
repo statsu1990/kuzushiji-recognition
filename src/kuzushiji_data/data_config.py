7f9c40fbf766a97e89f53a54c27008e440a3cc04
statsu1990 <47744788+statsu1990@users.noreply.github.com>
2019-10-26 02:16:26 +0900

    Update data_config.py
    modify the path.

--- src/kuzushiji_data/data_config.py
+++ src/kuzushiji_data/data_config.py
@@ -1,2 +1,2 @@
-data_dir = 'C:/Users/stnu2/Documents/Kaggle/kuzushiji-recognition/data'
-font_path = 'C:/Users/stnu2/Documents/Kaggle/kuzushiji-recognition/NotoSansCJKjp-hinted/NotoSansCJKjp-Regular.otf'
+data_dir = '../data'
+font_path = '../NotoSansCJKjp-hinted/NotoSansCJKjp-Regular.otf'
